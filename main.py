a8dcde75a0b39de8877c1a95b98d8f2ea7cca07b
luiz.winchester <luizwichester@gmail.com>
2025-03-27 17:06:44 -0300

    Feat: adição da pasta templates e importação no arquivo main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/home")
 def homepage():
-    return "Site no ar"
+    return render_template("homepage.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
