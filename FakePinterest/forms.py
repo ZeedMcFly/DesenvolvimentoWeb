1d60c77c781a0fba8496a38295256079a184ee53
luiz.winchester <luizwichester@gmail.com>
2025-03-31 16:14:05 -0300

    Feat: Adicionada a lógica dos formularios de login e criação de usuario

---
+++ FakePinterest/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length,ValidationError
+from FakePinterest.models import Usuario
+
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators =[DataRequired(), Email()] )
+    senha = PasswordField("Senha", validators = [DataRequired()])
+    botao_confirmacao = SubmitField("Fazer Login")
+
+
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators = [DataRequired(), Email()])
+    username = StringField("nome de usuario", validators = [DataRequired()])
+    senha = PasswordField("senha", validators = [DataRequired(), Length(6,20)])
+    confirmacao_senha = PasswordField("confirmação de Senha", validators =[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField ("Criar conta")
+
+    def validate_email(self,email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("Email já cadastrado")
