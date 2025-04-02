cb70d3734371fbd5c5b815cda0afc6f7ea53fc31
luiz.winchester <luizwichester@gmail.com>
2025-04-02 17:20:13 -0300

    feat: implementando na rota de login e criação de conta a lógica dos formularios e adição da rota de logout

--- FakePinterest/routes.py
+++ FakePinterest/routes.py
@@ -1,19 +1,42 @@
-from flask import render_template, url_for
-from FakePinterest import app
-from flask_login import login_required
+from flask import render_template, url_for, redirect
+from FakePinterest import app, database, bcrypt
+from flask_login import login_required, login_user, logout_user, current_user
 from FakePinterest.forms import FormLogin, FormCriarConta
+from FakePinterest.models import Usuario, Foto
+
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     formlogin = FormLogin()
+    if formlogin.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
+            login_user(usuario)
+            return redirect(url_for("perfil", usuario=usuario.username))
+        
     return render_template("homepage.html", form=formlogin)
 
 @app.route("/criarconta", methods=["GET", "POST"])
 def criarconta():
-    formcriarconta = FormCriarConta()
-    return render_template("criarconta.html", form=formcriarconta)
+    form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data,
+                          senha=senha, email=form_criarconta.email.data)
+        database.session.add(usuario)
+        database.session.commit()
+        login_user(usuario, remember=True)
+        return redirect(url_for("perfil", usuario=usuario.username))
+    return render_template("criarconta.html", form=form_criarconta)
 
 @app.route("/perfil/<usuario>")
 @login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("homepage"))
