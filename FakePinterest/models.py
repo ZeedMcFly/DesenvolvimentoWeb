02fd4b9c608b34c444df174be15ad1e290dde3af
luiz.winchester <luizwichester@gmail.com>
2025-04-02 17:22:46 -0300

    style: remoção de espaços

--- FakePinterest/models.py
+++ FakePinterest/models.py
@@ -8,11 +8,11 @@ def load_usuario(id_usuario):
     return Usuario.query.get(int(id_usuario))
 
 class Usuario(database.Model, UserMixin):
-    id = database.Column(database.Integer, primary_key = True)
-    username = database.Column(database.String, nullable = False)
-    email = database.Column(database.String, nullable = False, unique=True)
-    senha = database.Column(database.String, nullable = False)
-    fotos = database.relationship("Foto", backref= "usuario", lazy=True)
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+    fotos = database.relationship("Foto", backref="usuario", lazy=True)
 
 
 class Foto(database.Model):
