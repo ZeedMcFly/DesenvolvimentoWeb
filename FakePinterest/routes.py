335643bf1d71dc8cba8195a97a20f1cc56b214ef
luiz.winchester <luizwichester@gmail.com>
2025-04-03 14:59:47 -0300

    refactor: alteração para redirecionar a pagina com o id do usuario após o login ser concluído

--- FakePinterest/routes.py
+++ FakePinterest/routes.py
@@ -12,7 +12,7 @@ def homepage():
         usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
         if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
             login_user(usuario)
-            return redirect(url_for("perfil", usuario=usuario.username))
+            return redirect(url_for("perfil", id_usuario=usuario.id))
         
     return render_template("homepage.html", form=formlogin)
 
@@ -26,13 +26,17 @@ def criarconta():
         database.session.add(usuario)
         database.session.commit()
         login_user(usuario, remember=True)
-        return redirect(url_for("perfil", usuario=usuario.username))
+        return redirect(url_for("perfil", id_usuario=usuario.id))
     return render_template("criarconta.html", form=form_criarconta)
 
-@app.route("/perfil/<usuario>")
+@app.route("/perfil/<id_usuario>")
 @login_required
-def perfil(usuario):
-    return render_template("perfil.html", usuario=usuario)
+def perfil(id_usuario):
+    if int(id_usuario) == int(current_user.id):
+        return render_template("perfil.html", usuario=current_user)
+    else:
+        usuario = Usuario.query.get(int(id_usuario))
+        return render_template("perfil.html", usuario=usuario)
 
 
 @app.route("/logout")
